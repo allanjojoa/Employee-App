8e80cbb039aa10aee8a0a7fd075822b4b96360ce
Allan Jojo <allanjojoa@gmail.com>
2017-07-05 01:58:09 +0530

    db data from php

--- app/src/main/java/com/allan/android/employee/MainActivity.java
+++ app/src/main/java/com/allan/android/employee/MainActivity.java
@@ -2,12 +2,26 @@ package com.allan.android.employee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    static String total_result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void OnLogin(View view) {
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, "internship", "upsC734~");
+    }
+
+    public void setView(View view){
+        TextView total_textview=(TextView) findViewById(R.id.total_textview);
+        total_textview.setText(total_result);
+    }
+
 }
