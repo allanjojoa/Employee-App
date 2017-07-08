209431b90fb313fdd097dacbc657d60b2f726ada
Allan Jojo <allanjojoa@gmail.com>
2017-07-08 23:05:21 +0530

    Interface changed

--- app/src/main/java/com/allan/android/employee/BackgroundWorker.java
+++ app/src/main/java/com/allan/android/employee/BackgroundWorker.java
@@ -7,6 +7,7 @@ package com.allan.android.employee;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.view.View;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -34,13 +35,15 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
     @Override
     protected String doInBackground(String... params) {
         String type = params[0];
-        String login_url = "http://nitrobaba.com/internship/phpmysql.php";
+        String login_url = "http://nitrobaba.com/internship/count.php";
         if(type.equals("login")) {
             try {
 //                String user_name = params[1];
 //                String password = params[2];
-                String user_name="internship";
-                String password="upsC734~";
+                String query=params[1];
+                String query1=params[2];
+                String query2=params[3];
+                String lead_type=params[4];
                 URL url = new URL(login_url);
                 HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
@@ -48,8 +51,7 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
-                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                String post_data = URLEncoder.encode("query","UTF-8")+"="+URLEncoder.encode(query,"UTF-8")+"&" +URLEncoder.encode("query1","UTF-8")+"="+URLEncoder.encode(query1,"UTF-8")+"&" +URLEncoder.encode("query2","UTF-8")+"="+URLEncoder.encode(query2,"UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
@@ -58,8 +60,56 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
                 String result="";
                 String line="";
+                int num=1;
                 while((line = bufferedReader.readLine())!= null) {
                     result += line;
+                    if(lead_type.equals("ca")){
+                        if (num==1){
+                            MainActivity.ca_mca=line;
+                            num++;
+                        }
+                        else if(num==2){
+                            MainActivity.ca_b2b=line;
+                            num++;
+                        }
+                        else if (num==3){
+                            MainActivity.ca_total=line;
+                        }
+                    }
+                    else if(lead_type.equals("fl")){
+                        if (num==1){
+                            MainActivity.fl_mca=line;
+                            num++;
+                        }
+                        else if(num==2){
+                            MainActivity.fl_b2b=line;
+                            num++;
+                        }
+                        else if (num==3){
+                            MainActivity.fl_total=line;
+                        }
+
+                    }
+                    else if(lead_type.equals("tx")){
+                        if (num==1){
+                            MainActivity.tx_mca=line;
+                            num++;
+                        }
+                        else if(num==2){
+                            MainActivity.tx_b2b=line;
+                            num++;
+                        }
+                        else if (num==3){
+                            MainActivity.tx_total=line;
+                        }
+                    }
+                    else if(lead_type.equals("email")){
+                        if (num==1){
+                            MainActivity.total_email=line;
+                            num++;
+                        }
+                    }
+
                 }
                 bufferedReader.close();
                 inputStream.close();
@@ -86,13 +136,17 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
 
     @Override
     protected void onPostExecute(String result) {
-        alertDialog.setMessage("Done");
-        alertDialog.show();
-        MainActivity.total_result=result;
+        MainActivity.background_executed++;
+        if(MainActivity.background_executed==MainActivity.total_background){
+            alertDialog.setMessage("Done");
+            alertDialog.show();
+        }
+        //MainActivity.total_result=result;
     }
 
     @Override
     protected void onProgressUpdate(Void... values) {
         super.onProgressUpdate(values);
     }
+
 }
--- app/src/main/java/com/allan/android/employee/MainActivity.java
+++ app/src/main/java/com/allan/android/employee/MainActivity.java
@@ -6,22 +6,76 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    static String total_result;
+    static String ca_total,ca_b2b,ca_mca,fl_total,fl_b2b,fl_mca,tx_total,tx_b2b,tx_mca,total_email;
+    static int total_background=4,background_executed=1;
+    int i;
+    //private TextView ca_total_textview,ca_mca_textview,ca_b2b_textview,fl_total_textview,fl_mca_textview,fl_b2b_textview,tx_total_textview,tx_mca_textview,tx_b2b_textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ca_total_textview=(TextView) findViewById(R.id.ca_total_textview);
+//        ca_mca_textview=(TextView) findViewById(R.id.ca_mca_textview);
+//        ca_b2b_textview=(TextView) findViewById(R.id.ca_b2b_textview);
+//        fl_total_textview=(TextView) findViewById(R.id.fl_total_textview);
+//        fl_mca_textview=(TextView) findViewById(R.id.fl_mca_textview);
+//        fl_b2b_textview=(TextView) findViewById(R.id.fl_b2b_textview);
+//        tx_total_textview=(TextView) findViewById(R.id.tx_total_textview);
+//        tx_mca_textview=(TextView) findViewById(R.id.tx_mca_textview);
+//        tx_b2b_textview=(TextView) findViewById(R.id.tx_b2b_textview);
     }
 
     public void OnLogin(View view) {
         String type = "login";
+        String query="select count(*) from Auditdot_Internship.ca_feb9_2016 where doc_contact=7 and doneDate='2016-11-14' and leadtype='MCA'";;
+        String query1="select count(*) from Auditdot_Internship.ca_feb9_2016 where doc_contact=7 and doneDate='2016-11-14' and leadtype='B2B'";
+        String query2="select count(*) from Auditdot_Internship.ca_feb9_2016 where doc_contact=7 and doneDate='2016-11-14' ";
         BackgroundWorker backgroundWorker = new BackgroundWorker(this);
-        backgroundWorker.execute(type, "internship", "upsC734~");
+        backgroundWorker.execute(type,query,query1,query2, "ca");
+        query="select count(*) from Auditdot_Internship.fl_feb14_2016 where doc_contact=7 and doneDate='2016-11-14' and leadtype='MCA'";;
+        query1="select count(*) from Auditdot_Internship.fl_feb14_2016 where doc_contact=7 and doneDate='2016-11-14' and leadtype='B2B'";
+        query2="select count(*) from Auditdot_Internship.fl_feb14_2016 where doc_contact=7 and doneDate='2016-11-14' ";
+        BackgroundWorker backgroundWorker1 = new BackgroundWorker(this);
+        backgroundWorker1.execute(type,query,query1,query2, "fl");
+        query="select count(*) from Auditdot_Internship.tx_jan26_2016 where doc_contact=7 and doneDate='2016-11-14' and leadtype='MCA'";;
+        query1="select count(*) from Auditdot_Internship.tx_jan26_2016 where doc_contact=7 and doneDate='2016-11-14' and leadtype='B2B'";
+        query2="select count(*) from Auditdot_Internship.tx_jan26_2016 where doc_contact=7 and doneDate='2016-11-14' ";
+        BackgroundWorker backgroundWorker2 = new BackgroundWorker(this);
+        backgroundWorker2.execute(type,query,query1,query2, "tx");
+        query="select count(debtor_email) from Auditdot_Internship.tx_jan26_2016 where doc_contact=7 and doneDate='2016-11-14' and debtor_email and debtor_email IS NOT NULL or debtor_email <>''";
+        query1="select count(debtor_email) from Auditdot_Internship.tx_jan26_2016 where doc_contact=7 and doneDate='2016-11-14' and debtor_email and debtor_email IS NOT NULL or debtor_email <>''";
+        query2="select count(debtor_email) from Auditdot_Internship.tx_jan26_2016 where doc_contact=7 and doneDate='2016-11-14' and debtor_email and debtor_email IS NOT NULL or debtor_email <>''";
+        BackgroundWorker backgroundWorker3 = new BackgroundWorker(this);
+        backgroundWorker3.execute(type,query,query1,query2, "email");
+        //BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        //backgroundWorker.execute(type, "internship", "upsC734~");
     }
 
     public void setView(View view){
+        TextView ca_total_textview=(TextView) findViewById(R.id.ca_total_textview);
+        TextView ca_mca_textview=(TextView) findViewById(R.id.ca_mca_textview);
+        TextView ca_b2b_textview=(TextView) findViewById(R.id.ca_b2b_textview);
+        ca_total_textview.setText(ca_total);
+        ca_b2b_textview.setText(ca_b2b);
+        ca_mca_textview.setText(ca_mca);
+        TextView fl_total_textview=(TextView) findViewById(R.id.fl_total_textview);
+        TextView fl_mca_textview=(TextView) findViewById(R.id.fl_mca_textview);
+        TextView fl_b2b_textview=(TextView) findViewById(R.id.fl_b2b_textview);
+        fl_total_textview.setText(fl_total);
+        fl_b2b_textview.setText(fl_b2b);
+        fl_mca_textview.setText(fl_mca);
+        TextView tx_total_textview=(TextView) findViewById(R.id.tx_total_textview);
+        TextView tx_mca_textview=(TextView) findViewById(R.id.tx_mca_textview);
+        TextView tx_b2b_textview=(TextView) findViewById(R.id.tx_b2b_textview);
+        tx_total_textview.setText(tx_total);
+        tx_b2b_textview.setText(tx_b2b);
+        tx_mca_textview.setText(tx_mca);
+        i=Integer.parseInt(ca_total.toString())+Integer.parseInt(tx_total.toString())+Integer.parseInt(fl_total.toString());
         TextView total_textview=(TextView) findViewById(R.id.total_textview);
-        total_textview.setText(total_result);
+        total_textview.setText(":"+i);
+        TextView email_textview=(TextView) findViewById(R.id.email_textview);
+        email_textview.setText(total_email);
     }
 
 }
